c12d41c4d2e87f2d7187c95c385fb3dd50540092
reidn3r <reidnersteam@hotmail.com>
2024-09-05 20:56:23 -0300

    Fix retry method

--- src/providers/axios/axios.provider.ts
+++ src/providers/axios/axios.provider.ts
@@ -1,6 +1,8 @@
 import { provide, StatusCode } from "@expressots/core";
 import axios from 'axios'; 
 import { IAxiosProvider } from "./IAxios.provider";
+import { retryAsync } from "ts-retry";
+import { request } from "http";
 
 @provide(AxiosProvider)
 export class AxiosProvider implements IAxiosProvider{
@@ -11,35 +13,38 @@ export class AxiosProvider implements IAxiosProvider{
     private readonly maxDelay:number = 1000; //delay: 1s
 
     async isAuthorized():Promise<boolean>{
-        return this.retry(async() => {
-            const request = await axios.get(this.authorizeUrl.toString());
-            return request.status == StatusCode.OK;
-        })
-    }
-
-    async notify():Promise<boolean>{
-        return this.retry(async() => {
-            const request = await axios.post(this.notifyUrl.toString());
-            return request.status == StatusCode.NoContent;
-        });
-    }
-
-    private async retry(fn: () => Promise<boolean>, availableAttemps:number = this.maxRetries):Promise<boolean>{
-        const result:boolean = await fn();
-        
-        if(result) return true;
-        
-        if(!result && availableAttemps > 0){
-            await this.delay(this.maxDelay);
-            return this.retry(fn, availableAttemps - 1);
+        try{
+            await retryAsync(async() => {
+                const request = await axios.get(this.authorizeUrl.toString());
+                return request;
+            },{
+                delay: this.maxDelay,
+                maxTry: this.maxRetries,
+                until: (request) => request.status === StatusCode.OK
+            });
+            return true;
         }
-        else{
+        catch(err:any){
             return false;
         }
     }
 
-    private async delay(timeMs:number):Promise<void>{
-        return new Promise(r => setTimeout(r, timeMs));
+    async notify():Promise<boolean>{
+        try{
+            await retryAsync(async() => {
+                const request = axios.post(this.notifyUrl.toString());
+                return request;
+            }, {
+                delay: this.maxDelay,
+                maxTry: this.maxRetries,
+                until: (r) => r.status === StatusCode.NoContent
+            })
+            return true;
+        }
+        catch(err:any){
+            return false;
+        }
     }
+
 
 }
