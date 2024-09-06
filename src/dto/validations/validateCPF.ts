007904cd6a70dcd3630cfea60c017ace4c0d5d0c
reidn3r <reidnersteam@hotmail.com>
2024-09-06 08:13:57 -0300

    fix: cpf validation basic algorithm

--- src/dto/validations/validateCPF.ts
+++ src/dto/validations/validateCPF.ts
@@ -3,7 +3,25 @@ import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface 
 @ValidatorConstraint({ name: "ValidateCPF", async: false })
 export class ValidateCPF implements ValidatorConstraintInterface {
     validate(cpf: string | null) {
-        return cpf?.length === 11;
+        if (!cpf || cpf.length !== 11) return false;
+        let digits:number[] = cpf.split("").map(Number);
+        let sum:number = 0;
+        let q:number = 0;
+    
+        for(let i=10; i > 1; i--){
+            sum += digits[10-i] * i;
+        }
+        
+        q = (sum * 10) % 11 == 10 ? 0 : (sum * 10) % 11;
+        if(q != digits[9]) return false;
+        
+        sum = 0;
+        for(let i = 11; i > 1; i--){
+            sum += digits[11-i]*i;
+        }
+        q = (sum * 10) % 11 == 10 ? 0 : (sum * 10) % 11;
+        if(q != digits[10]) return false;
+        return true;
     }
 
     defaultMessage(validationArguments?: ValidationArguments): string {
